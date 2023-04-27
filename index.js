27207c66246ad074b2546a8586180f3306736e04
Mr. li <54256440+lixiaxopeng0@users.noreply.github.com>
2023-04-27 16:29:09 +0800

    Update index.js
    按键联动添加

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const keyboardSpace = document.getElementsByClassName('keyboard-space')[0];
+const showInput = document.getElementsByClassName('show-input')[0];
+
+// 展示字符串
+let showString = '';
+// caps lock是否锁住
+let isCapsLock = false;
 // 创建按键
 function keysCreate({ keys, className, parent }) {
     const functionSpace = document.createElement('div');
@@ -16,7 +22,7 @@ function keysCreate({ keys, className, parent }) {
 
 // 上方功能行按键
 function functionKeysCreate() {
-    const fKeys = Array.from({ length: 12 })?.map((_, index) => `F1${index + 1}`);
+    const fKeys = Array.from({ length: 12 })?.map((_, index) => `F${index + 1}`);
     const keys = ['esc', ...fKeys, 'close'];
     keysCreate({ keys, className: 'keyboard-function-space', parent: keyboardSpace })
 }
@@ -27,26 +33,26 @@ function joinStr(item) {
 // 数字行按键
 function numberKeysCreate() {
     const fkeysArray = [
-        ['~', '`'], ['!', 1], ['@', 2], ['#', 3], ['$', 4],
-        ['%', 5], ['^', 6], ['&', 7], ['*', 8], ['(', 9], [')', 0],
-        ['_', '-'], ['+', '='], ['', 'delete'],
+        ['~', '`', 'backquote'], ['!', 1, 'digit1'], ['@', 2, 'digit2'], ['#', 3, 'digit3'], ['$', 4, 'digit4'],
+        ['%', 5, 'digit5'], ['^', 6, 'digit6'], ['&', 7, 'digit7'], ['*', 8, 'digit8'], ['(', 9, 'digit9'], [')', 0, 'digit0'],
+        ['_', '-', 'minus'], ['+', '=', 'equal'], ['', 'delete', 'delete'],
     ];
-    const keys = fkeysArray.map(item => ({ content: joinStr(item), value: item[1] }));
+    const keys = fkeysArray.map(item => ({ content: joinStr(item), value: item[2] }));
     keysCreate({ keys, className: 'keyboard-number-space', parent: keyboardSpace })
 }
 
 // 字母按键
 function wordKeysCreate() {
     // 第一行
-    const fkeysArrayOne = [['{', '['], ['}', ']'], ['|', '\\']].map(item => ({ content: joinStr(item), value: item[1] }));
+    const fkeysArrayOne = [['{', '[', 'bracketLeft'], ['}', ']', 'bracketRight'], ['|', '\\', 'backslash']].map(item => ({ content: joinStr(item), value: item[2] }));
     const keysOne = [{ content: joinStr(['', 'tab']), value: 'tab' }, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', ...fkeysArrayOne];
     keysCreate({ keys: keysOne, className: 'keyboard-word-space-one', parent: keyboardSpace });
     // 第二行
-    const fkeysArrayTwo = [[':', ';'], [`"`, `'`], ['', 'enter']].map(item => ({ content: joinStr(item), value: item[1] }));
+    const fkeysArrayTwo = [[':', ';', 'semicolon'], [`"`, `'`, 'quote'], ['', 'enter', 'enter']].map(item => ({ content: joinStr(item), value: item[2] }));
     const keysTwo = [{ content: joinStr(['', 'caps lock']), value: 'caps-lock' }, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ...fkeysArrayTwo];
     keysCreate({ keys: keysTwo, className: 'keyboard-word-space-two', parent: keyboardSpace });
     // 第三行
-    const fkeysArrayThree = [['<', ','], [`>`, `.`], ['?', '/'], ['', 'shift']].map(item => ({ content: joinStr(item), value: item[1] }));
+    const fkeysArrayThree = [['<', ',', 'comma'], [`>`, `.`, 'period'], ['?', '/', 'slash'], ['', 'shift', 'shift']].map(item => ({ content: joinStr(item), value: item[2] }));
     const keysThree = [{ content: joinStr(['', 'shift']), value: 'shift-left' }, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ...fkeysArrayThree];
     keysCreate({ keys: keysThree, className: 'keyboard-word-space-three', parent: keyboardSpace });
 }
@@ -65,9 +71,9 @@ function positionKeysCreate() {
     const position = document.getElementsByClassName('position')[0];
     position.innerHTML = '';
     // 上∨
-    keysCreate({ keys: [{ content: '↑', value: 'top' }], className: 'position-top', parent: position });
+    keysCreate({ keys: [{ content: '↑', value: 'up' }], className: 'position-up', parent: position });
     // 左 下 右
-    const positionKeys = [['left', '←'], ['bottom', '↓'], ['right', '→']].map(item => ({ content: item[1], value: item[0] }))
+    const positionKeys = [['left', '←'], ['down', '↓'], ['right', '→']].map(item => ({ content: item[1], value: item[0] }))
     keysCreate({ keys: positionKeys, className: 'position-other', parent: position });
 }
 function executeCreateKeys() {
@@ -77,9 +83,92 @@ function executeCreateKeys() {
     assistKeysCreate();
     positionKeysCreate();
 }
+
+// 辅助按键，不显示
+const assistArray = [
+    'AltLeft', 'ControlLeft', 'MetaRight', 'MetaLeft', 'AltRight',
+    'ShiftLeft', 'ShiftRight', 'Escape', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight',
+    'CapsLock', 'Tab', 'fn', 'close', 'shift',
+    ...Array.from({ length: 12 })?.map((_, index) => `F${index + 1}`),
+];
+
+// 文案显示处理
+function showText({exclude = assistArray, type, value}) {
+    switch (true) {
+        case exclude.includes(type):
+            break;
+        case type === 'Backspace':
+            showString = showString.slice(0, -1);
+            showInput.value = showString;
+            break;
+        case type === 'Enter':
+            showString = '';
+            showInput.value = showString;
+            break;
+        default:
+            showString += value;
+            showInput.value = showString;
+            break;
+    }
+}
+
+// 键盘监听
+// class操作
+function classNameOperate({className, type = 'down', isMouse = false}) {
+    const key = document.getElementsByClassName(className)[0];
+    // lock特殊
+    if (className === 'caps-lock') {
+        key.classList.add('key-down');
+        isCapsLock = !isCapsLock;
+        isCapsLock && key.classList.add('locked');
+        !isCapsLock && key.classList.remove('locked');
+        setTimeout(() => {
+            key.classList.remove('key-down');
+        }, 100);
+        return;
+    }
+
+    type === 'down' && key.classList.add('key-down');
+    !isMouse && type === 'up' && key.classList.remove('key-down');
+    // 鼠标点击、和tab按键，自动消除className
+    (isMouse || type === 'down' && className === 'tab') && setTimeout(() => {
+        key.classList.remove('key-down');
+    }, 100);
+}
+
+function keyboardListen(keyType, e) {
+    let type = e.code;
+    let value = e.key;
+    let className = KEY_TYPE[type];
+    classNameOperate({className, type: keyType});
+    if (keyType === 'down') {
+        showText({type, value});
+    }
+}
+
+// 点击按键
+// 判读是否是字母
+const reg = /^[a-zA-Z]{1}$/;
+function click(e) {
+    const content = e.target.textContent?.split('\n');
+    let value = '';
+    const className = e.target.classList[0];
+    const type = CLASS_TYPE?.[className];
+    // 方向按键，外部，点击不显示
+    if (content?.length <= 2 && content[0]?.length < 10 && !className.includes('position')) {
+        value = content?.length === 1 ? content[0] : content[1];
+        value = type === 'Space' ? ' ' : value;
+        value = reg.test(value) && !isCapsLock ? value.toLowerCase() : value;
+        showText({type, value});
+        classNameOperate({className, type: 'down', isMouse: true});
+    }
+}
 // 初始化
 function init() {
     executeCreateKeys();
+    document.addEventListener('keydown', keyboardListen.bind(this, 'down'));
+    document.addEventListener('keyup', keyboardListen.bind(this, 'up'));
+    keyboardSpace.addEventListener('click', click);
 }
 init();
 
